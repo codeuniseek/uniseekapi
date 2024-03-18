f4783454cc623017aeec2d099e9e7091a8bd4d4f
codeuniseek <hireme.uniseek@gmail.com>
2024-03-19 02:01:31 +0545

    added data

--- courses/admin.py
+++ courses/admin.py
@@ -2,4 +2,25 @@ from django.contrib import admin
 from courses.models import Course
 from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Course, ImportExportModelAdmin)
+
+class CourseAdmin(ImportExportModelAdmin):
+    list_display = [
+        "university_name",
+        "course_name",
+        "gross_tuition_fee",
+        "earliest_intake",
+        "program_duration",
+        "program_type",
+        "state",
+        "application_deadline",
+    ]
+    list_filter = [
+        "university_name",
+        "program_type",
+        "state",
+        "earliest_intake",
+        "course_name",
+    ]
+
+
+admin.site.register(Course, CourseAdmin)
